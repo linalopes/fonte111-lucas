cde58777c145a97505a1f5597046d1da9c31d28c
Thiago Hersan <thiago.hersan+github@gmail.com>
2020-04-03 15:04:10 -0300

    usando dicionario para simplificar organização das imagens

--- fonte111Lucas.js
+++ fonte111Lucas.js
@@ -1,33 +1,33 @@
 let input, button, greeting;
-let A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, X, Y, W, Z;
+let images = {};
 
 function preload() {
-  A = loadImage('data/A.png');
-  B = loadImage('data/B.png');
-  C = loadImage('data/C.png');
-  D = loadImage('data/D.png');
-  E = loadImage('data/E.png');
-  F = loadImage('data/F.png');
-  G = loadImage('data/G.png');
-  H = loadImage('data/H.png');
-  I = loadImage('data/I.png');
-  J = loadImage('data/J.png');
-  K = loadImage('data/K.png');
-  L = loadImage('data/L.png');
-  M = loadImage('data/M.png');
-  N = loadImage('data/N.png');
-  O = loadImage('data/O.png');
-  P = loadImage('data/P.png');
-  Q = loadImage('data/Q.png');
-  R = loadImage('data/R.png');
-  S = loadImage('data/S.png');
-  T = loadImage('data/T.png');
-  U = loadImage('data/U.png');
-  V = loadImage('data/V.png');
-  W = loadImage('data/W.png');
-  X = loadImage('data/X.png');
-  Y = loadImage('data/Y.png');
-  Z = loadImage('data/Z.png');
+  images['A'] = loadImage('data/A.png');
+  images['B'] = loadImage('data/B.png');
+  images['C'] = loadImage('data/C.png');
+  images['D'] = loadImage('data/D.png');
+  images['E'] = loadImage('data/E.png');
+  images['F'] = loadImage('data/F.png');
+  images['G'] = loadImage('data/G.png');
+  images['H'] = loadImage('data/H.png');
+  images['I'] = loadImage('data/I.png');
+  images['J'] = loadImage('data/J.png');
+  images['K'] = loadImage('data/K.png');
+  images['L'] = loadImage('data/L.png');
+  images['M'] = loadImage('data/M.png');
+  images['N'] = loadImage('data/N.png');
+  images['O'] = loadImage('data/O.png');
+  images['P'] = loadImage('data/P.png');
+  images['Q'] = loadImage('data/Q.png');
+  images['R'] = loadImage('data/R.png');
+  images['S'] = loadImage('data/S.png');
+  images['T'] = loadImage('data/T.png');
+  images['U'] = loadImage('data/U.png');
+  images['V'] = loadImage('data/V.png');
+  images['W'] = loadImage('data/W.png');
+  images['X'] = loadImage('data/X.png');
+  images['Y'] = loadImage('data/Y.png');
+  images['Z'] = loadImage('data/Z.png');
 }
 
 function setup() {
@@ -38,7 +38,7 @@ function setup() {
   button = createButton('ok');
   button.position(input.x + input.width, 100);
   button.mousePressed(greet);
-  
+
   greeting = createElement('h2', 'Qual seu nome?');
   greeting.position(20, 5);
 }
@@ -47,89 +47,12 @@ function greet() {
   const name = input.value().toUpperCase();
   greeting.html('Olá ' + name + '! Sua coreografia da Pablo é assim:');
   input.value('');
+  const iY = input.y + input.height;
   clear();
+
   for (let i=0; i<name.length; i++) {
-    //text(name[i],width/2+4*i, height/4);
-    switch (name[i]) {
-    case 'a':
-      image(A, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'b':
-      image(B, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'c':
-      image(C, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'd':
-      image(D, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'e':
-      image(E, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'f':
-      image(F, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'g':
-      image(G, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'h':
-      image(H, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'i':
-      image(I, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'j':
-      image(J, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'k':
-      image(K, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'l':
-      image(L, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'm':
-      image(M, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'n':
-      image(N, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'o':
-      image(O, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'p':
-      image(P, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'q':
-      image(Q, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'r':
-      image(R, 100*i, height/2, 100, 177.7778);
-      break;
-    case 's':
-      image(S, 100*i, height/2, 100, 177.7778);
-      break;
-    case 't':
-      image(T, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'u':
-      image(U, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'v':
-      image(V, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'w':
-      image(W, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'x':
-      image(x, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'y':
-      image(Y, 100*i, height/2, 100, 177.7778);
-      break;
-    case 'z':
-      image(Z, 100*i, height/2, 100, 177.7778);
-      break;
-    }
+    let letter = name[i];
+    image(images[letter], 100*i, iY, 100, 177.7778);
   }
 
 
